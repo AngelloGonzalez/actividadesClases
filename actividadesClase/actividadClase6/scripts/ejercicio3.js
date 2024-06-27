7596c96f341b3f9a507ef1fefa35a1036e3f5d4c
Angelo Gonzalez <angellog9714@gmail.com>
2024-06-27 14:51:41 -0500

    actualizacion ejercicios js

---
+++ actividadesClase/actividadClase6/scripts/ejercicio3.js
@@ -0,0 +1,13 @@
+console.log('Este es mi Ejercicio 3');
+
+// CICLOS 
+
+
+/*
+3.) Mostrar números:
+Escribe un programa que use un bucle for para imprimir los números del 1 al 10 en la consola.
+*/
+
+for(let i=1; i<=10; i++){
+    console.log(i);
+}
